11a373016aa4a0398c1c0b222685088b29f3b6a8
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-04-25 07:22:50 +0700

    jobs page snackbar added

--- src/pages/jobs/jobs.ts
+++ src/pages/jobs/jobs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-jobs',
@@ -7,11 +7,36 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class JobsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  bookmark = false;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JobsPage');
+  }
+
+  changeBookmark(){
+    // ini nnti kao udah berhasil bookmark ada snackbarnya gitu 
+    if(this.bookmark == false){
+      this.bookmark = true;
+      let toast = this.toastCtrl.create({
+        message: 'Bookmark success',
+        duration: 3000
+      });
+      toast.present();
+    }
+    else{
+      this.bookmark = false;
+      let toast = this.toastCtrl.create({
+        message: 'Remove bookmark success',
+        duration: 3000
+      });
+      toast.present();
+    }
   }
 
 }
