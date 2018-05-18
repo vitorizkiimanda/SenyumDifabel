3039a670ead9fe79457d911a72c73710500a4f42
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-05-18 14:37:31 +0700

    job detail page

--- src/pages/job-detail/job-detail.ts
+++ src/pages/job-detail/job-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-job-detail',
@@ -7,11 +7,40 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class JobDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  bookmark:boolean = false;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JobDetailPage');
+  }
+
+  changeBookmark(){
+    // ini nnti kao udah berhasil bookmark ada snackbarnya gitu 
+    if(this.bookmark == false){
+      this.bookmark = true;
+      let toast = this.toastCtrl.create({
+        message: 'Bookmark success',
+        duration: 3000,
+        showCloseButton: true,
+        closeButtonText: 'Ok'
+      });
+      toast.present();
+    }
+    else{
+      this.bookmark = false;
+      let toast = this.toastCtrl.create({
+        message: 'Remove bookmark success',
+        showCloseButton: true,
+        closeButtonText: 'Ok',
+        duration: 3000
+      });
+      toast.present();
+    }
   }
 
 }
