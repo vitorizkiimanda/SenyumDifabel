d91aa5bd610ec1851e1f959930d471468156f30a
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-05-18 15:00:48 +0700

    edit profile page

--- src/pages/profile-edit/profile-edit.ts
+++ src/pages/profile-edit/profile-edit.ts
@@ -7,6 +7,14 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ProfileEditPage {
 
+  name:any="Vito Rizki Imanda"
+  email:any="vitorizkiimanda@gmail.com"
+  phone:any="089657011491"
+  address:any="South Jakarta"
+  biodata:any="lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet"
+  experience:any="lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet lorem ipsum dolor sir amet"
+  
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
