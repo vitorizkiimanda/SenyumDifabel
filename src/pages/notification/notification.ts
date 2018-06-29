cd8f430aeff00b94bcb089e526245b7843d821fd
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-06-29 08:55:27 +0700

    Dynamic notificaton UI

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { SuperTabsController } from 'ionic2-super-tabs';
+import { ProfileOtherPage } from '../profile-other/profile-other';
 
 
 @Component({
@@ -9,9 +10,13 @@ import { SuperTabsController } from 'ionic2-super-tabs';
 })
 export class NotificationPage {
 
+  
+  statusFollow : boolean = true;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
+    public alertCtrl: AlertController,
     private superTabsCtrl: SuperTabsController) {
   }
 
@@ -24,4 +29,34 @@ export class NotificationPage {
     this.superTabsCtrl.showToolbar(true);
     this.superTabsCtrl.enableTabsSwipe(true);
   }
+
+  openProfile(){
+    this.navCtrl.push(ProfileOtherPage);
+  }
+
+  follow(){
+    this.statusFollow = false;
+  }
+
+  unfollow(){
+    let prompt = this.alertCtrl.create({
+      subTitle:"Unfollow NamaOrangnya?",
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            // this.deleteJob(dataJob);
+          }
+        },
+        {
+          text: 'Unfollow',
+          handler: data => {
+            this.statusFollow = true;
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
 }
