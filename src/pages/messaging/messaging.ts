ac941c4d31bc08d7812bebaeed6d76db9b00fdbb
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-07-12 01:53:06 +0700

    minor bug fix

--- src/pages/messaging/messaging.ts
+++ src/pages/messaging/messaging.ts
@@ -1,25 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { ContactPage } from '../contact/contact';
 import { MessagePersonalPage } from '../message-personal/message-personal';
 import { MessageGroupPage } from '../message-group/message-group';
 import { CreateGroupPage } from '../create-group/create-group';
+import { Data } from '../../providers/data';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'page-messaging',
   templateUrl: 'messaging.html',
 })
 export class MessagingPage {
+  user_id:any;
+  user_email:any;
 
   messages:any;
   list_search: any;
   statusSearch : boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  chats:any;
+
+  constructor(
+    public navCtrl: NavController,
+    private data : Data,
+    public navParams: NavParams,
+    public loadCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public http: Http
+  ) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MessagingPage');
+  ionViewWillEnter() {
+    this.data.getData().then((data) => {
+      this.user_email = data.user_email;
+      this.user_id = data.user_id;
+
+      this.getChats(this.user_id);
+    })
   }
 
   openContact(){
@@ -34,6 +52,43 @@ export class MessagingPage {
   openCreateGroup(){
     this.navCtrl.push(CreateGroupPage);
   }
+
+
+  getChats(data){
+    this.data.getOriginalPassword().then((password) =>{
+      console.log(password);
+      let headers = new Headers({'Authorization':'Basic ' +  btoa(this.user_email + ':' +password) });
+      this.http.get(this.data.BASE_URL+"auth/listChat/"+data,{ headers: headers }).timeout(5000).subscribe(data => {
+        let response = data.json();
+        console.log("chats",response);
+        this.chats = response;
+        // alert(response)
+
+      }, err => {     
+        console.log("error cui :",err);
+        this.runTimeError();
+        
+      });
+    })
+  }
+
+  runTimeError(){
+    let alert = this.alertCtrl.create({
+      title: 'Failed',
+      subTitle: 'Please try again',     
+      buttons: [
+        {
+          text: 'Refresh',
+          handler: data => {
+            this.ionViewWillEnter();
+          }
+        }
+      ] 
+    });
+    alert.present();
+  }
+
+
 
   //Fungsi Searchbar
   getItems(ev) {
